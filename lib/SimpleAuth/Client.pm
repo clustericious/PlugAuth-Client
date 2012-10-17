80f0b746ee993daa455e7c77d869b60d7806831f
Graham Ollis <perl@wdlabs.com>
2012-10-17 13:31:05 -0400

    upgrade to use route_args and document

--- lib/SimpleAuth/Client.pm
+++ lib/SimpleAuth/Client.pm
@@ -3,28 +3,40 @@ package SimpleAuth::Client;
 use strict;
 use warnings;
 use Log::Log4perl qw(:easy);
-
 use Clustericious::Client;
 
 our $VERSION = '0.08';
 
-route 'welcome'   => 'GET', '/';
-route 'auth'      => 'GET', '/auth';
-route_doc 'authz' => "user action resource";
-route 'resources' => 'GET', '/authz/resources', \("user action resource_regex");
-route 'host_tag'  => 'GET', '/host', \("host tag");
-route 'groups'    => 'GET', '/groups', \("user");
-route 'actions'   => 'GET', '/actions';
-route 'user'      => 'GET', '/user';
-route 'group'     => 'GET', '/group';
-route 'users'     => 'GET', '/users', \("group");
-route 'create_user'  => 'POST', '/user', \("--user user --password password");
-route 'delete_user'  => 'DELETE',  '/user', \("user");
-route 'create_group' => 'POST', '/group', \("--group group --users user1,user2,...");
-route 'delete_group' => 'DELETE', '/group', \("group");
+route welcome      => 'GET', '/';
+route auth         => 'GET', '/auth';
+route_doc authz    => "user action resource";
+route resources    => 'GET', '/authz/resources', \("user action resource_regex");
+route host_tag     => 'GET', '/host', \("host tag");
+route groups       => 'GET', '/groups', \("user");
+route actions      => 'GET', '/actions';
+route user         => 'GET', '/user';
+route group        => 'GET', '/group';
+route users        => 'GET', '/users', \("group");
+route delete_user  => 'DELETE',  '/user', \("user");
+route delete_group => 'DELETE', '/group', \("group");
+route_doc 'grant'  => 'group action resource';
+
+route create_user => 'POST', '/user', \("--user username --password password");
+route_args create_user => [
+  { name => 'user',     type => '=s', required => 1 },
+  { name => 'password', type => '=s', required => 1 },
+];
+
+route create_group => 'POST', '/group', \("--group group --users user1,user2,...");
+route_args create_user => [
+  { name => 'group', type => '=s', required => 1 },
+  { name => 'users', type => '=s', required => 1 },
+];
 
 route_doc 'update_group' => 'group --users user1,user2,...';
-route_doc 'grant' => 'group action resource';
+route_args create_user => [
+  { name => 'users', type => '=s', required => 1 },
+];
 
 sub authz
 {
@@ -32,6 +44,8 @@ sub authz
 
   my $url = Mojo::URL->new( $self->server_url );
 
+  $resource = "/$resource" unless $resource =~ m{^/};
+  
   $url->path("/authz/user/$user/$action$resource");
 
   $self->_doit('GET', $url);
@@ -133,6 +147,98 @@ On the command line :
 =head1 DESCRIPTION
 
 This module provides a perl front-end to the SimpleAuth API.
+
+=head1 METHODS
+
+=head2 $client-E<gt>auth
+
+Returns true if the SimpleAuth server can authenticate the user.  
+Username and passwords can be specified with the login method or
+via the application's configuration file, see L<Clustericious::Client>
+for details.
+
+=head2 $client-E<gt>authz($user $action, $resource)
+
+Returns true if the given user ($user) is authorized to perform the
+given action ($action) on the given resource ($resource).
+
+=head2 $client-E<gt>host_tag($ip_address, $tag)
+
+Returns true if the host specified by the given IP address ($ip_address)
+has the given host tag ($tag).
+
+=head2 $client-E<gt>groups($user)
+
+Returns a list reference containing the groups that the given user ($user)
+belongs to.
+
+=head2 $client-E<gt>actions
+
+Returns a list reference containing the actions that the SimpleAuth server
+knows about.
+
+=head2 $client-E<gt>user
+
+Returns a list reference containing all usernames.
+
+=head2 $client-E<gt>group
+
+Returns a list reference containing all group names.
+
+=head2 $client-E<gt>users($group)
+
+Returns the list of users belonging to the given group ($group).
+
+=head2 $client-E<gt>create_user( \%args )
+
+Create a user with the given username and password.
+
+=over 4
+
+=item * username
+
+The new user's username
+
+=item * password
+
+The new user's password
+
+=back
+
+=head2 $client-E<gt>delete_user( $username )
+
+Delete the user with the given username.
+
+=head2 $client-E<gt>create_group( \%args )
+
+Create a group.
+
+=over 4
+
+=item * group
+
+The name of the new group
+
+=item * users
+
+Comma separated list (as a string) of the users that
+should initially belong to this group.
+
+=back
+
+=head2 $client-E<gt>update_group( $group, '--users' => $users )
+
+Update the given group ($group) replacing the existing list with
+the new list ($users), wihch is a comma separated list as a string.
+
+=head2 $client-E<gt>delete_group( $group )
+
+Delete the given group ($group).
+
+=head2 $client-E<gt>grant( $user, $action, $resource )
+
+Grants the given user ($user) the authorization to perform the given
+action ($action) on the given resource ($resource).
 
 =head1 SEE ALSO
 
